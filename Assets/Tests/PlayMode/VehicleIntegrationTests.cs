97c92ad38d64a623e842f2e55ee2244fc0fc2f64
Legion <totes.mickgoats@gmail.com>
2026-03-15 11:20:53 -0600

    test: add parameterized drive layout power routing tests

--- Assets/Tests/PlayMode/VehicleIntegrationTests.cs
+++ Assets/Tests/PlayMode/VehicleIntegrationTests.cs
@@ -343,5 +343,100 @@ namespace R8EOX.Tests.PlayMode
                 }
             }
         }
+
+
+        // ---- Drive Layout (power routing) ----
+
+        [TestCase(R8EOX.Vehicle.Drivetrain.DriveLayout.RWD, 2)]
+        [TestCase(R8EOX.Vehicle.Drivetrain.DriveLayout.AWD, 4)]
+        [Test]
+        public void UpdateLayout_DriveLayout_SetsCorrectMotorWheelCount(
+            R8EOX.Vehicle.Drivetrain.DriveLayout layout, int expectedMotorCount)
+        {
+            var (dt, front, rear, root) = CreateDrivetrainTestRig(layout);
+            try
+            {
+                int motorCount = dt.UpdateLayout(front, rear);
+
+                Assert.AreEqual(expectedMotorCount, motorCount,
+                    $"Layout {layout} should activate {expectedMotorCount} motor wheels");
+
+                bool frontShouldBeMotor = (layout == R8EOX.Vehicle.Drivetrain.DriveLayout.AWD);
+                foreach (var w in front)
+                    Assert.AreEqual(frontShouldBeMotor, w.IsMotor,
+                        $"Front wheel IsMotor should be {frontShouldBeMotor} for {layout}");
+                foreach (var w in rear)
+                    Assert.IsTrue(w.IsMotor, $"Rear wheel should always be motor");
+            }
+            finally
+            {
+                Object.DestroyImmediate(root);
+            }
+        }
+
+        [TestCase(R8EOX.Vehicle.Drivetrain.DriveLayout.RWD)]
+        [TestCase(R8EOX.Vehicle.Drivetrain.DriveLayout.AWD)]
+        [Test]
+        public void Distribute_DriveLayout_OnlyExpectedWheelsReceivePower(
+            R8EOX.Vehicle.Drivetrain.DriveLayout layout)
+        {
+            var (dt, front, rear, root) = CreateDrivetrainTestRig(layout);
+            try
+            {
+                dt.UpdateLayout(front, rear);
+                dt.Distribute(26f, front, rear);
+
+                if (layout == R8EOX.Vehicle.Drivetrain.DriveLayout.RWD)
+                {
+                    foreach (var w in front)
+                        Assert.AreEqual(0f, w.MotorForceShare, 0.001f,
+                            "RWD: front wheels must receive zero motor force");
+                    foreach (var w in rear)
+                        Assert.AreNotEqual(0f, w.MotorForceShare,
+                            "RWD: rear wheels must receive non-zero motor force");
+                }
+                else
+                {
+                    foreach (var w in front)
+                        Assert.AreNotEqual(0f, w.MotorForceShare,
+                            "AWD: front wheels must receive non-zero motor force");
+                    foreach (var w in rear)
+                        Assert.AreNotEqual(0f, w.MotorForceShare,
+                            "AWD: rear wheels must receive non-zero motor force");
+                }
+            }
+            finally
+            {
+                Object.DestroyImmediate(root);
+            }
+        }
+
+        private (R8EOX.Vehicle.Drivetrain dt, R8EOX.Vehicle.RaycastWheel[] front,
+                 R8EOX.Vehicle.RaycastWheel[] rear, GameObject root)
+            CreateDrivetrainTestRig(R8EOX.Vehicle.Drivetrain.DriveLayout layout)
+        {
+            var root = new GameObject("DrivetrainTestRig");
+
+            var drivetrainObj = new GameObject("Drivetrain");
+            drivetrainObj.transform.SetParent(root.transform, false);
+            var dt = drivetrainObj.AddComponent<R8EOX.Vehicle.Drivetrain>();
+            dt.ActiveDriveLayout = layout;
+
+            var front = new R8EOX.Vehicle.RaycastWheel[2];
+            var rear = new R8EOX.Vehicle.RaycastWheel[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                var fObj = new GameObject($"WheelF{i}");
+                fObj.transform.SetParent(root.transform, false);
+                front[i] = fObj.AddComponent<R8EOX.Vehicle.RaycastWheel>();
+
+                var rObj = new GameObject($"WheelR{i}");
+                rObj.transform.SetParent(root.transform, false);
+                rear[i] = rObj.AddComponent<R8EOX.Vehicle.RaycastWheel>();
+            }
+
+            return (dt, front, rear, root);
+        }
     }
 }
